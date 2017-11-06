aa79702fcb43d1d525979a19353e4398a93e5877
Scott <scott@internaught.io>
2017-11-05 22:14:06 -0800

    uhhhhhhhhhh
    uhhhhhhhhhh

--- massgrep.py
+++ massgrep.py
@@ -12,7 +12,7 @@ ssh = paramiko.SSHClient()
 ssh.load_system_host_keys()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ssh.connect('shell.iad3a.mlsrvr.com',username='scot8965')
+ssh.connect('host.com',username='user')
 
 stdin, stdout, stderr = ssh.exec_command(command)
 
